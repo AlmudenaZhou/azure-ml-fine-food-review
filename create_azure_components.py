8f8af33e0bba8e9aa147424c85c5cce16bcde14c
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-11-24 00:22:10 +0100

    Added logger to create_azure_components

--- create_azure_components.py
+++ create_azure_components.py
@@ -1,12 +1,17 @@
+import logging.config
 from dotenv import load_dotenv
 
 from src.pipeline_steps.text_processing.create_text_processing_component import create_text_processing_component
 from src.pipeline_steps.training_data_cleaning.create_training_data_cleaning_component import create_training_data_cleaning_component
 
 
+logging.config.fileConfig('logger.conf')
+logger = logging.getLogger(__name__)
+
+
 def main():
     create_training_data_cleaning_component()
-    # create_text_processing_component()
+    create_text_processing_component()
 
 
 if __name__ == "__main__":
