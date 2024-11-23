1a53cb8c091604dab83aecf719744bf432ad49ab
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-11-24 00:51:52 +0100

    Bugfix: check of the compute instance status

--- run_azure_components.py
+++ run_azure_components.py
@@ -1,8 +1,8 @@
 import os
 import logging.config
 
+from src.pipeline_steps.text_processing.create_text_processing_component import run_text_processing_component
 from src.azure_ml_interface import AzureMLInterface
-from src.pipeline_steps.split_data.create_split_data_component import run_text_processing_component
 from src.pipeline_steps.training_data_cleaning.create_training_data_cleaning_component import run_training_data_cleaning_component
 
 
@@ -13,9 +13,9 @@ def main():
     azure_ml_interface = AzureMLInterface()
     compute_name = os.getenv("COMPUTE_INSTANCE_NAME")
     comp_status = azure_ml_interface.get_compute_status(compute_name)
-
-    if comp_status == 'stopped':
-        azure_ml_interface.start_compute()
+    logger.info("Compute instance Status:", comp_status.state)
+    if comp_status.state == 'Stopped':
+        azure_ml_interface.start_compute(compute_name)
 
     run_training_data_cleaning_component(wait_for_completion=True)
     run_text_processing_component(wait_for_completion=True)
