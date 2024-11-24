ed8a99cd561735d49375b5e3518f76779b1eeb53
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-11-24 01:36:32 +0100

    Added create_split_data_component to the create components workflow

--- create_azure_components.py
+++ create_azure_components.py
@@ -1,6 +1,7 @@
 import logging.config
 from dotenv import load_dotenv
 
+from src.pipeline_steps.split_data.manage_split_data_component import create_split_data_component
 from src.pipeline_steps.text_processing.manage_text_processing_component import create_text_processing_component
 from src.pipeline_steps.training_data_cleaning.manage_training_data_cleaning_component import create_training_data_cleaning_component
 
@@ -12,6 +13,7 @@ logger = logging.getLogger(__name__)
 def main():
     create_training_data_cleaning_component()
     create_text_processing_component()
+    create_split_data_component()
 
 
 if __name__ == "__main__":
