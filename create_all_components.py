c024cfe732ea85c352a7f3ae13a163d1e886d796
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-11-29 15:57:26 +0100

    Added the inference pipeline to create_all_components

--- create_all_components.py
+++ create_all_components.py
@@ -1,6 +1,9 @@
 import logging.config
 from dotenv import load_dotenv
 
+from src.inference_pipeline.model_prediction.manage_model_prediction_component import create_model_prediction_inference_component
+from src.inference_pipeline.text2vector.manage_text2vector_component import create_text2vector_inference_component
+from src.inference_pipeline.text_processing.manage_text_processing_component import create_text_processing_inference_component
 from src.training_pipeline.model_training.manage_model_training_component import create_model_training_component
 from src.training_pipeline.handle_imbalance.manage_handle_imbalance_component import create_handle_imbalance_component
 from src.training_pipeline.text2vector.manage_text2vector_component import create_text2vector_component
@@ -16,6 +19,8 @@ logger = logging.getLogger(__name__)
 def create_all_components():
     logger.info("Stating creating all components")
     create_training_components()
+
+    create_inference_components()
 
 
 def create_training_components():
@@ -38,6 +43,15 @@ def create_training_components():
     create_model_training_component()
     logger.info("Finished model training component")
     logger.info("Finished creating training components")
+
+
+def create_inference_components():
+
+    create_text_processing_inference_component()
+
+    create_text2vector_inference_component()
+
+    create_model_prediction_inference_component()
 
 
 def main():
