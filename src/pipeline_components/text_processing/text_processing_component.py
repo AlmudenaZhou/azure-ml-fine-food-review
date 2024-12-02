a4e1f8f65aaed576c02fc5bae66e450421b07544
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-12-02 14:21:25 +0100

    Added text processing component updated with the new pipeline format

--- src/pipeline_components/text_processing/text_processing_component.py
+++ src/pipeline_components/text_processing/text_processing_component.py
@@ -9,7 +9,7 @@ from text_processing_step import TextPreprocessingStep
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_data_folder", type=str, help="name of the input file data")
+    parser.add_argument("--input_data_folder", type=str, help="path to data")
     parser.add_argument("--input_data_filename", type=str, help="name of the input file data")
     parser.add_argument("--output_data_folder", type=str, help="path to posprocessed data")
     parser.add_argument("--output_data_filename", type=str, help="path to posprocessed data",
