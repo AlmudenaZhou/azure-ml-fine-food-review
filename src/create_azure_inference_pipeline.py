963d4021452ca52ff35f4eb54a9d5bca8e7210ea
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-12-06 00:52:59 +0100

    Added the pipeline as component in create_azure_inference_pipeline

--- src/create_azure_inference_pipeline.py
+++ src/create_azure_inference_pipeline.py
@@ -1,18 +1,20 @@
 import os
 
 from azure.ai.ml.constants import AssetTypes
-from azure.ai.ml import dsl, Input
+from azure.ai.ml import Input
+from azure.ai.ml.dsl import pipeline
 
-from src.training_pipeline.register_model.register_model_step import RegisterModelStep
+from src.local_components.register_model.register_model_step import RegisterModelStep
 from src.tools.azure_ml_interface import AzureMLInterface
 
 
-@dsl.pipeline(
+@pipeline(
     compute="serverless",
     description="E2E data_prep-train pipeline",
 )
 def fine_food_reviews_inference_pipeline(
-    pipeline_job_data_input,
+    pipeline_job_input_folder: Input,
+    pipeline_job_input_file: str,
     text_processing_filename="infer_processed_data.csv",
     text2vector_filename="infer_text2vec_data.csv",
     text2vector_modelname="text2vector_model.pkl",
@@ -26,19 +28,13 @@ def fine_food_reviews_inference_pipeline(
     text2vector_comp = azure_ml_interface.get_component("text2vector")
 
     train_predict_model_comp = azure_ml_interface.get_component("train_predict_model")
-    
-    pipeline_job_data_folder, pipeline_job_data_filename = os.path.split(pipeline_job_data_input.path)
-
-    print("---------------------------------------")
-    print(pipeline_job_data_folder, pipeline_job_data_filename)
 
     text_processing_job = text_processing_comp(
-        input_data_folder=pipeline_job_data_folder,
-        input_data_filename=pipeline_job_data_filename,
+        input_data_folder=pipeline_job_input_folder,
+        input_data_filename=pipeline_job_input_file,
         output_data_filename=text_processing_filename
     )
 
-    print()
     text2vector_job = text2vector_comp(
         model_input_path=Input(type=AssetTypes.CUSTOM_MODEL, path=text2vec_model.id),
         model_filename=text2vector_modelname,
@@ -68,14 +64,18 @@ def main():
 
     pipeline_job_data_input = (f"azureml://subscriptions/{subscription_id}/resourcegroups/{resource_group}/workspaces/"
                                + f"{workspace}/datastores/workspaceblobstore/paths/{relative_raw_data_uri}")
-    pipeline_job_data_input = Input(type="uri_file", path=pipeline_job_data_input)
     
-    pipeline_job = fine_food_reviews_inference_pipeline(pipeline_job_data_input)
+    pipeline_job_input_folder, pipeline_job_input_file = os.path.split(pipeline_job_data_input)
+    pipeline_job_input_folder = Input(type="uri_folder", path=pipeline_job_input_folder)
+    
+    pipeline_job = fine_food_reviews_inference_pipeline(pipeline_job_input_folder, pipeline_job_input_file)
 
     environment_variables = {"TEXT_COLNAME": os.getenv("TEXT_COLNAME"),
                              "TARGET": os.getenv("TARGET")}
 
     azure_ml_interface = AzureMLInterface()
+
+    azure_ml_interface.create_component_from_component(fine_food_reviews_inference_pipeline())
     azure_ml_interface.register_pipeline_from_job_pipeline(pipeline_job, environment_variables=environment_variables)
 
 
@@ -86,9 +86,9 @@ if __name__ == "__main__":
 
     model_params = {
         "text2vec": {"model_path": text2vec_model_path, "model_name": "fine-food-reviews-text2vec", 
-                     "model_version": "3", "model_type": AssetTypes.CUSTOM_MODEL},
+                     "model_version": "4", "model_type": AssetTypes.CUSTOM_MODEL},
         "pred": {"model_path": pred_model_path, "model_name": "fine-food-reviews-predictor",
-                 "model_version": "3", "model_type": AssetTypes.CUSTOM_MODEL}
+                 "model_version": "4", "model_type": AssetTypes.CUSTOM_MODEL}
     }
     models = RegisterModelStep().main(model_params_dict=model_params)
     text2vec_model, pred_model = models["text2vec"], models["pred"]
